e2272c87cea0447c0b5673b30dcfc7af953dd02b
ALEXANDRE123KATSARAVA <alexandrekatsarava@gmail.com>
2023-07-14 02:11:56 +0400

    day3

---
+++ python/day3/homework/homework.py
@@ -0,0 +1,50 @@
+mother = ("sofia"
+father = ("davit"
+brother = ("misho"
+sister = ("keti"
+me= ("alexandre"
+grandmother= ("qetino"
+grandfather = ("nugzari"
+greatgrandmother = ("nona"
+grandmother = ("veta"
+grandfather = ("misha"
+uncle1 = ("vano"
+uncle2 = ("gio"
+uncle3 = ("dato"
+aunt = ("ani"
+aunt2 = ("nino"
+aunt3 = ("jana"
+cousin1 = ("dachi"
+cousin2 = ("kato"
+cousin3 = ("natali"
+cousin4 = ("sebastian"
+cousin5 ("luka")
+cousin6 = ("sandro")
+gamingisbad = (fortnite)
+gamingisbad = (pubgmobile)
+gamingisbad = (COD)
+gamingisbad = (farcry)
+gamingisbad = (CSGO)
+gamingisbad = (ALL.THE.GAMES.YOU.CAN.PLAY)
+print (mother)
+print (father)
+print (brother)
+print (sister)
+print (me)
+print (grandmother)
+print (grandfather)
+print (greatgrandmother)
+print (grandmother)
+print (grandfather)
+print (unlce1)
+print (uncle2)
+print (uncle3)
+print (aunt)
+print (aunt2)
+print (aunt3)
+print (cousin1)
+print (cousin2)
+print (cousin3)
+print (cousin4)
+print (cousin5)
+print (cousin6)
