5737d3285d093873b6d42dfd2fd3bc20f3f62720
ALEXANDRE123KATSARAVA <139178047+ALEXANDRE123KATSARAVA@users.noreply.github.com>
2023-07-14 20:15:09 +0400

    Add files via upload

---
+++ wrong.py
@@ -0,0 +1,18 @@
+name = alex) 
+age = 12)
+country = georgia)
+city = tbilisi)
+siblings = 2
+sister = qeti)
+brother = misho)
+father = david)
+mother = sofia)
+print name
+print age
+print country
+print city
+print (siblings)
+print (sister)
+print (brother)
+print (father)
+print (mother)
